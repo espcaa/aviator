b7a003079c39661c4ae79310a559c3594ffbf987
spectralo <alice@spectralo.me>
2025-06-23 01:53:35 +0200

    added a bunch of things

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,9 +1,25 @@
 import { View, Text } from "react-native";
 
 export default function HomeScreen() {
+  let MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
+
+  if (!MAPBOX_TOKEN) {
+    console.warn(
+      "MAPBOX_TOKEN is not set. Please set it in your environment variables.",
+    );
+  }
+
   return (
-    <View>
-      <Text>Welcome to the Home Screen!</Text>
+    <View
+      style={{
+        justifyContent: "center",
+        flex: 1,
+      }}
+      className="bg-background-0"
+    >
+      <View style={{ marginHorizontal: 30 }}>
+        <Text>Welcome to the Home Screen!</Text>
+      </View>
     </View>
   );
 }
